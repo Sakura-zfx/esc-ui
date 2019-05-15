8beabc16d8f001f93a22a715d85abba546648a5c
yangming <472102644@qq.com>
2019-05-15 16:29:03 +0800

    feat: dot

---
+++ packages/utils/dot.ts
@@ -0,0 +1,71 @@
+import { isIOS, online, isDef, cookieGet } from './index'
+
+interface Options {
+  moduleId?: string
+  eventId?: string
+  orgId?: string
+  userId?: string
+  platform?: 'iOS' | 'android'
+}
+
+const defaultOptions: Options = {
+  moduleId: undefined,
+  eventId: undefined,
+  orgId: cookieGet('orgId'),
+  userId: cookieGet('userId'),
+  platform: isIOS ? 'iOS' : 'android'
+}
+
+export default class Dot {
+  options: Options = {}
+
+  constructor (options?: Options) {
+    if (options && typeof options !== 'object') {
+      throw new Error('Dot constructor options is a object.')
+    } else if (!isDef(options)) {
+      options = {}
+    }
+    this.options = {
+      ...defaultOptions,
+      ...options
+    }
+  }
+
+  getUrl (didArr: string[] | void): string {
+    let { platform, moduleId, eventId, userId, orgId } = this.options
+    if (didArr) {
+      const [ ,, mid, eid ] = didArr
+      moduleId = mid || moduleId
+      eventId = eid || eventId
+    }
+    const base = online ? 'https://admin.jituancaiyun.com' : 'http://admin.jituancaiyun.net'
+    return `${base}/dot-log/logExt.json?platform=${platform}&mid=${moduleId}&eid=${eventId}&uid=${userId || 1}${orgId ? `&orgId=${orgId}` : ''}`
+  }
+
+  /**
+   * @params did {String} 打点字符 形如 '0.0.4405.1'
+  */
+  hit (did?: string) {
+    let didArr: string[] | void
+    if (did) {
+      didArr = did.split('.').slice(0, 4)
+      if (didArr.length < 4) {
+        didArr = new Array(4 - didArr.length).fill('').concat(didArr)
+      }
+    }
+    const url = this.getUrl(didArr)
+    let img: HTMLImageElement | null = new Image()
+    img.src = url
+    img.onload = img.onerror = () => {
+      img = null
+    }
+  }
+}
+
+const dot = new Dot()
+export const hit = (did: string) => {
+  if (!isDef(did)) {
+    throw new Error('did is a string, such as "0.0.4405.1"')
+  }
+  dot.hit(did)
+}
--- packages/utils/index.ts
+++ packages/utils/index.ts
@@ -1,18 +1,16 @@
 import bem from './bem'
-// import { isDef } from './util'
-
 import { VNode } from 'vue/types'
-
-export function isVNode (node: VNode | string | void): boolean {
-  return node !== null &&
-    typeof node === 'object' &&
-    Object.prototype.hasOwnProperty.call(node, 'componentOptions')
-}
 
 export function use (name: string) {
   return [
     bem(name)
   ]
+}
+
+export function isVNode (node: VNode | string | void): boolean {
+  return node !== null &&
+    typeof node === 'object' &&
+    Object.prototype.hasOwnProperty.call(node, 'componentOptions')
 }
 
 export function isDef<T> (val: T): boolean {
@@ -22,4 +20,23 @@ export function isDef<T> (val: T): boolean {
 export function formatNumber (val: number, fixed: number): string {
   const pow = Math.pow(10, fixed)
   return (Math.round(pow * val) / pow).toFixed(fixed)
+}
+
+export const isIOS = /iPhone/i.test(navigator.userAgent)
+
+export const online = /\.com/.test(location.origin)
+
+export const cookieGet = (name: string): string => {
+  let cookieValue = ''
+  if (document.cookie && document.cookie !== '') {
+    let cookies = document.cookie.split(';')
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i].trim()
+      if (cookie.substring(0, name.length + 1) === `${name}=`) {
+        cookieValue = decodeURIComponent(cookie.substring(name.length + 1))
+        break
+      }
+    }
+  }
+  return cookieValue
 }
